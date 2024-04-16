2343a2a20ec2f2e7259dd2ccf4e9b8a1246550ea
Henderson Reed Hummel <reed.hummel@gmail.com>
2024-04-16 15:26:01 -0700

    working example backend - handles submission of links, retrieving text based on semantic search.

--- antilink/main.py
+++ antilink/main.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
+from readability import Document
+from txtai.embeddings import Embeddings
 import requests
+from nanoid import generate
+
 
 app = Flask(__name__)
+embeddings = Embeddings(path="sentence-transformers/nli-mpnet-base-v2", content=True, objects=True)
 
 
 @app.route("/")
@@ -12,13 +17,38 @@ def hello_world():
 @app.route('/api/v1/submit', methods=['PUT'])
 def submit_url():
     data = request.get_json()
-    if 'url' in data:
-        url = data['url']
-        data = requests.get(url)
-        breakpoint()
-        return jsonify({"message": "URL submitted successfully", "html": "test"})
-    else:
-        return jsonify({"error": "Invalid JSON data"}), 400
+    if 'url' not in data:
+        return jsonify({"error": "Invalid JSON data, please include a URL."}), 400
+    url = data['url']
+    data = requests.get(url)
+    entry_object = {"text": Document(data.content).summary(), "url": url}
+    entry_id = generate()
+
+    embeddings.upsert([(entry_id, entry_object)])
+    return jsonify({"id": entry_id})
+
+
+@app.route('/api/v1/query_string', methods=['PUT'])
+def query_string():
+    """
+    Accepts a string, returns N top results.
+    The request data needs to look like
+        {'query': 'the string to search for in the index', 'limit': N} where N is the maximum number of allowed results.
+    """
+    data = request.get_json()
+    if 'query' not in data or 'limit' not in data:
+        return jsonify({"error": "Invalid JSON data, please include a query and a limit."}), 400
+
+    results = embeddings.search(data['query'], data['limit'])
+    return jsonify(results)
+
+
+@app.route('/api/v1/query_id', methods=['GET'])
+def query_url():
+    """
+    Accepts an id of a URL that's in the DB, returns N top results.
+    """
+    raise NotImplementedError("This endpoint is not implemented")
 
 
 def dev():
--- antilink/test/test_main.py
+++ antilink/test/test_main.py
@@ -1,10 +1,36 @@
 import requests
-import json
+
+base_url = "http://localhost:5000/api/v1"
 
 
-def test_basic_submit():
-    api_url = 'http://localhost:5000/api/v1/submit'
-    json_payload = {"url": "http://example.com"}
-    response = requests.put(api_url, json=json_payload)
-    breakpoint()
-    print(response.json())
+def test_basic_workflow():
+    """
+    This ensures that the
+        1. basic "submit" functionality is working,
+        2. we can query
+
+    wherein a url gets submitted and the response is a positive status
+    code and a snip of json with an id.
+    """
+
+    # basic submit functionality
+    api_url = f'{base_url}/submit'
+    json_payload1 = {"url": "http://example.com"}
+    json_payload2 = {"url": "https://python.org"}
+    submit_response1 = requests.put(api_url, json=json_payload1)
+    submit_response2 = requests.put(api_url, json=json_payload2)
+    assert submit_response1.status_code == 200
+    assert submit_response2.status_code == 200
+    assert 'id' in submit_response1.json()
+    assert 'id' in submit_response2.json()
+
+    # does the retrieval work as expected?
+    api_url = f'{base_url}/query_string'
+    json_payload = {"query": "example", 'limit': 1}
+    query_response = requests.put(api_url, json=json_payload)
+    assert submit_response1.json()['id'] == query_response.json()[0]['id']
+
+    api_url = f'{base_url}/query_string'
+    json_payload = {"query": "Python", 'limit': 1}
+    query_response = requests.put(api_url, json=json_payload)
+    assert submit_response2.json()['id'] == query_response.json()[0]['id']
